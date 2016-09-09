959eecc1b09372c5fafe03f930d4603375961416
hahahipo <b95643twtw@gmail.com>
2016-09-09 11:28:16 +0800

    v1

--- app/src/main/java/org/iii/www/myapplicationeric/MainActivity001.java
+++ app/src/main/java/org/iii/www/myapplicationeric/MainActivity001.java
@@ -2,6 +2,7 @@ package org.iii.www.myapplicationeric;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity001 extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity001 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main001);
+        Log.i("Brad","v1");
     }
 }
